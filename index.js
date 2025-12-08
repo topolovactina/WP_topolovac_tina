8048c021ad1aacd2be397d35c4e71161b5a212f5
Tina Topolovac <topolovactina@gmail.com>
2025-12-08 16:03:41 +0100

    08.12.2025.
    grid, js,....

---
+++ index.js
@@ -0,0 +1,32 @@
+//JAVA SCRIPT//
+// Google , desni klik, provjeri, source, novi fajl, index.js
+//alert ("ovo je moja prva JS stranica"); 
+//prompt("unesi svoje ime");
+//let ime = prompt("unesi svoje ime");
+//alert ("tvoje ime je: " +ime )
+//let broj = prompt("unesi jedan broj");
+//alert ("kvadrat toga broja je: " +(broj*broj) );
+//let broj1 = prompt("unesi prvi broj") 
+//let broj2 = prompt("unesi drugi broj")
+//alert ("razlika dva broja je: " +(broj1 - broj2) );
+//usporedba
+//let broj1 =prompt("unesi prvi broj");
+//let broj2 =prompt("unesi drugi broj");
+//if (broj1 > broj2)
+    //alert (broj1 + "je veći od" + broj2 );
+//else if ( broj2 > broj1)
+   // alert (broj2 +"je veći od" + broj1 );
+//else
+   // alert (broj1 +"je jednak broju" + broj2);
+
+
+//BMI App 
+
+//BMI = masa(kg) / visina*visina (m)
+//-unjeti masu i visinu
+//-ispisati bmi (Vaš BMI iznosi: xx)
+
+//let masa = prompt("unesi svoju masu u kg");
+//let visina = prompt ("unesi svoju visinu u metrima");
+//let bmi = masa / (visina * visina);
+//alert ("Vaš BMI iznosi: " + bmi   .toFixed(2) );
