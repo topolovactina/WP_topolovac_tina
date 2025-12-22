417c328fac0ba9e247f822153e8c63e609b55c79
Tina Topolovac <topolovactina@gmail.com>
2025-12-22 13:33:38 +0100

    kolokvij 2
    kolokvij2

---
+++ KOLOKVIJ 2/index.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    let broj = prompt("Unesite jedan broj:");
+    let rezultat = broj * 2;
+    alert("Unio si broj " + broj + ", a dva puta veći broj je " + rezultat);
+};
