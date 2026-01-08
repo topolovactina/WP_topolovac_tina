5ea0222fd7b1dfcd1ba98b0045aac461e798edd8
Tina Topolovac <topolovactina@gmail.com>
2026-01-08 17:53:46 +0100

    ZADAĆA 6
    zadaća broj 6

---
+++ ZADACE/zadaca6/zadaca6.js
@@ -0,0 +1,12 @@
+// Unos broja od korisnika
+let broj = prompt("Unesite broj:");
+
+// Pretvaranje unosa u broj
+broj = Number(broj);
+
+// Provjera je li broj veći od 10
+if (broj > 10) {
+    alert("Broj je veći od 10");
+} else {
+    alert("Broj nije veći od 10");
+}
